2ed0ed8b8909d10c3dcdd0c902783991db27eda6
Petr Drastil <petr.drastil@gmail.com>
2023-08-13 18:00:59 +0200

    Replace GPG with SSH signing

--- workspaces/default/run/before/00-static.sh
+++ workspaces/default/run/before/00-static.sh
@@ -4,4 +4,10 @@ DOTFILES=${DOTFILES:-"$HOME/.dotfiles"}
 source "${DOTFILES}/lib/setup.sh"
 
 title "Fonts"
-rsync "$DOTFILES"/static/fonts/*.ttf "$HOME"/Library/Fonts/
+rsync "$DOTFILES"/static/fonts/*.ttf "$HOME/Library/Fonts/"
+
+title "SSH config"
+if [[ ! -d "$HOME/.ssh" ]]; then
+  mkdir -p "$HOME/.ssh" && chmod 700 "$HOME/.ssh"
+  rsync -r "$DOTFILES"/static/ssh/* "$HOME/.ssh/"
+fi
